1cea87c46411a677cf3049a582e3a9974807ef5c
Enes Ozcan <enes.ozcan@hazelcast.com>
2020-11-23 11:50:32 +0300

    Add client setup to guide.

--- server2/src/main/java/com/hazelcast/guide/HazelcastApplication.java
+++ server2/src/main/java/com/hazelcast/guide/HazelcastApplication.java
@@ -5,6 +5,8 @@ import javax.enterprise.inject.Produces;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
+import com.hazelcast.client.HazelcastClient;
+import com.hazelcast.client.config.ClientConfig;
 import com.hazelcast.config.Config;
 import com.hazelcast.config.MapConfig;
 import com.hazelcast.core.Hazelcast;
@@ -16,7 +18,17 @@ public class HazelcastApplication extends Application {
 
     static String MAP_NAME = "guide-map";
 
+    //Use Hazelcast client to connect an existing cluster
     @Produces
+    HazelcastInstance create() {
+        ClientConfig clientConfig = new ClientConfig();
+        // configure existing Hazelcast cluster address to be connected
+        clientConfig.getNetworkConfig().addAddress("127.0.0.1:5701");
+        return HazelcastClient.newHazelcastClient(clientConfig);
+    }
+
+    //Create Hazelcast instance along with the application
+    /*@Produces
     HazelcastInstance create() {
         Config config = new Config();
         // all other configurations (networking,
@@ -26,5 +38,5 @@ public class HazelcastApplication extends Application {
         mapConfig.setTimeToLiveSeconds(30);
         config.addMapConfig(mapConfig);
         return Hazelcast.newHazelcastInstance(config);
-    }
+    }*/
 }
